396eafb86ff902fce00d9fe60939326097404c66
rakib25 <mislam171229@bscse.uiu.ac.bd>
2023-02-17 18:17:32 +0600

    adding skills

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,9 +35,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9ba651041a97228874241046aca5bb7e",
-"/": "9ba651041a97228874241046aca5bb7e",
-"main.dart.js": "0878f261d370875a42d35c6979960f51",
+"index.html": "0cd47f4cf8b7d650b4db8e2be69fb408",
+"/": "0cd47f4cf8b7d650b4db8e2be69fb408",
+"main.dart.js": "8ed0a7ac5a156973ff7fae99329913f4",
 "manifest.json": "e8b93911c990aea179dc297b73a980d5",
 "version.json": "961f48960438b7c81295c43961109072"
 };
